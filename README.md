a3df20c622705a4ab26dfad5254161e21f447482
justinwoo <moomoowoo@gmail.com>
2020-01-14 23:15:47 +0200

    update README and deps

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 packages.dhall to local psc-package set
 
 If you don't know what this is, then don't worry about it.
+
+## requirements
+
+See shell.nix for details
+
+* purs
+* psc-package
+* dhall
+* dhall-json
--- default.nix
+++ default.nix
@@ -3,6 +3,29 @@
 let
   dynamic-linker = pkgs.stdenv.cc.bintools.dynamicLinker;
 
+  easy-ps = import (
+    pkgs.fetchFromGitHub {
+      owner = "justinwoo";
+      repo = "easy-purescript-nix";
+      rev = "cd549a1fb33252f3fb2d807d4c8ea4a19903230f";
+      sha256 = "1vl0h5k0bcn2v6b10sj2jjyvnzvq16qfq527g1xi3gzjx7ylfs0g";
+    }
+  ) {
+    inherit pkgs;
+  };
+
+  easy-dhall = import (
+    pkgs.fetchFromGitHub {
+      owner = "justinwoo";
+      repo = "easy-dhall-nix";
+      rev = "1aa3b306e49cada554d8acfe3902e27527ecef98";
+      sha256 = "1hhm4m6y5iavfx088qbifn76jdrgglkgqvmgki8z6hj5bz5mnh06";
+    }
+  ) {
+    inherit pkgs;
+  };
+
+
 in
 pkgs.stdenv.mkDerivation rec {
   name = "soba";
@@ -28,6 +51,14 @@ pkgs.stdenv.mkDerivation rec {
     patchelf $TARGET \
       --interpreter ${dynamic-linker} \
       --set-rpath ${libPath}
+
+    wrapProgram $TARGET \
+      --prefix PATH : ${pkgs.lib.makeBinPath [
+        easy-ps.purs
+        easy-ps.psc-package
+        easy-dhall.dhall-simple
+        easy-dhall.dhall-json-simple
+      ]}
   '';
 
   dontInstall = true;
